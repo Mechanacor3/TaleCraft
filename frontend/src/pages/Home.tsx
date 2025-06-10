f3cb3116d97031e922de98e9a59cb562a4da813d
Mechanacor3 <70974571+Mechanacor3@users.noreply.github.com>
2025-06-10 14:16:26 -0400

    Merge main into work

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,18 +1,12 @@
-import React from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import StoryInput from '../components/StoryInput';
+import React from "react";
+import StoryInput from "../components/StoryInput";
 
 const Home: React.FC = () => {
-    return (
-        <div className="flex flex-col min-h-screen">
-            <Header />
-            <main className="flex-grow flex items-center justify-center">
-                <StoryInput />
-            </main>
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <StoryInput />
+    </div>
+  );
 };
 
 export default Home;
--- frontend/src/pages/Outline.tsx
+++ frontend/src/pages/Outline.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import StoryInput from '../components/StoryInput';
-import StoryOutline from '../components/StoryOutline';
+import React from "react";
+import StoryInput from "../components/StoryInput";
+import StoryOutline from "../components/StoryOutline";
 
 const Outline: React.FC = () => {
-    return (
-        <div className="flex flex-col items-center p-4">
-            <h1 className="text-2xl font-bold mb-4">Interactive Story Outline</h1>
-            <StoryInput />
-            <StoryOutline />
-        </div>
-    );
+  return (
+    <div className="flex flex-col items-center p-4">
+      <h1 className="text-2xl font-bold mb-4">Interactive Story Outline</h1>
+      <StoryInput />
+      <StoryOutline />
+    </div>
+  );
 };
 
 export default Outline;
